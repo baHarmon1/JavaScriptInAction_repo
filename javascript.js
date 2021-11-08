a0db72d1daead910da5775fbbc39d11dac952916
Brycen Harmon <brycenharmon@gmail.com>
2021-11-08 16:32:03 -0600

    on arrays section of worksheet

---
+++ javascript.js
@@ -0,0 +1,84 @@
+// variables
+// -----------------------------------------------------
+// let dayOfWeek = "Monday"
+// console.log(dayOfWeek)
+
+// dayOfWeek = "Friday"
+// console.log(`I cannot wait for ${dayOfWeek}!`)
+
+// let animalInput = prompt("What is your favorite animal?")
+// let colorInput = prompt("What is your favorite color?")
+
+// console.log(`Oh wow! I have never seen a ${colorInput} ${animalInput}!`)
+
+// Conditionals
+// -----------------------------------------------------
+// Favorite Breakfast: Biscuits and Gravy!
+// Favorite Lunch: Burger?
+// Favorite Dinner: Chinese food!
+
+//  let timeOfDay = 1300
+//  let favoriteMeal = ""
+ 
+//  if (timeOfDay < 1200) {
+//      favoriteMeal = "Biscuits and Gravy"
+//  } else if (timeOfDay >= 1200 && timeOfDay < 1700) {
+//      favoriteMeal = "Burger"
+//  } else if (timeOfDay >= 1700) {
+//      favoriteMeal = "Chinese Food"
+//  }
+//  console.log(favoriteMeal)
+// -----------------------------------------------------
+// Random Number
+// randomNumberOneTen = Math.floor(Math.random() * 10)
+
+// if (randomNumberOneTen >= 0 && randomNumberOneTen <= 2) {
+//     console.log("Beatles")
+// } else if (randomNumberOneTen >= 3 && randomNumberOneTen <= 5) {
+//     console.log("Stones")
+// } else if (randomNumberOneTen >= 6 && randomNumberOneTen <= 8) {
+//     console.log("Floyd")
+// } else if (randomNumberOneTen >= 9 && randomNumberOneTen <= 10) {
+//     console.log("Hendrix")
+// }
+// -----------------------------------------------------
+
+// for (let i = 0; i < 7; i++) {
+//     console.log("JavaScript is cool")
+// }
+
+// for (i = 0; i < 11; i++) {
+//     console.log(i)
+// }
+
+// for (i = 0; i <= 10; i++) {
+//     if (i === 0) {
+//         console.log(i + "Hello")
+//             continue
+//     } else if (i % 2 === 0) {
+//         console.log(i + "Hello")
+//     } else {
+//         console.log(i + "Goodbye")
+//     }
+// }
+
+// -----------------------------------------------------
+// Favorite Movie
+
+// let favoriteMovie = "Moana"
+// console.log(favoriteMovie)
+
+// Favorite Band
+
+// function askUserFavoriteBand() {
+//     let favoriteBand = prompt("What is your favorite band?");
+//     return favoriteBand
+// }
+// let userFavoriteBand = askUserFavoriteBand();
+
+// function concertDisplay(musicalAct) {
+//     let myStreet = prompt("What street do you live on?");
+//     console.log(`It would be great if ${musicalAct} played a concert on ${myStreet}.`);
+// }
+// concertDisplay(userFavoriteBand)
+
